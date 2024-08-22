5537ae619665349451daa3ef8354636d2d34edf6
XardasDark <dennispetana@gmail.com>
2024-08-22 19:21:10 +0200

    Forgot to change init

--- cowspirit/__init__.py
+++ cowspirit/__init__.py
@@ -32,6 +32,7 @@ from .cowspirit import CowSpirit
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
-
 async def setup(bot: Red) -> None:
+    cow = CowSpirit(bot)
     await bot.add_cog(CowSpirit(bot))
+    await cow.initialize()
