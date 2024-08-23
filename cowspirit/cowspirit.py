eb6efa288ad76f6a0d352a9333f6a5bb142c8178
XardasDark <dennispetana@gmail.com>
2024-08-23 15:32:39 +0200

    Changed role_id

--- cowspirit/cowspirit.py
+++ cowspirit/cowspirit.py
@@ -14,7 +14,7 @@ class CowSpirit(commands.Cog):
         super().__init__()
         self.bot = bot
         self.channel_id = 1276521873030774804
-        self.role_id = 1274141906913202186
+        self.role_id = 1276530764883689618
         self.scheduler = AsyncIOScheduler()
         self.scheduler_running = False  # Track whether the scheduler is active
 
